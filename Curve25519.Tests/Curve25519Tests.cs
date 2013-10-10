673fb2b9327ca92fe2dcdc29b8ee7bf9e8cfbe53
Hans Wolff <hans.wolff@gmx.de>
2013-10-10 11:08:50 +0100

    initial commit

---
+++ Curve25519.Tests/Curve25519Tests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Elliptic.Tests
+{
+    [TestFixture]
+    public class Curve25519Tests
+    {
+        [Test]
+        public void DiffieHellmanSuccess()
+        {
+            Random random = TestHelpers.CreateSemiRandomGenerator(); // not truly random in case we need to reproduce test values
+
+            for (int i = 0; i < 1000; i++)
+            {
+                byte[] alicePrivate = Curve25519.ClampPrivateKey(TestHelpers.GetRandomBytes(random, 32));
+                byte[] alicePublic = Curve25519.GetPublicKey(alicePrivate);
+
+                byte[] bobPrivate = Curve25519.ClampPrivateKey(TestHelpers.GetRandomBytes(random, 32));
+                byte[] bobPublic = Curve25519.GetPublicKey(bobPrivate);
+
+                byte[] aliceShared = Curve25519.GetSharedSecret(alicePrivate, bobPublic);
+                byte[] bobShared = Curve25519.GetSharedSecret(bobPrivate, alicePublic);
+
+                Assert.AreEqual(aliceShared, bobShared);
+            }
+        }
+
+        [Test]
+        public void DiffieHellmanFail()
+        {
+            Random random = TestHelpers.CreateSemiRandomGenerator();
+            for (int i = 0; i < 1000; i++)
+            {
+                byte[] alicePrivate = Curve25519.ClampPrivateKey(TestHelpers.GetRandomBytes(random, 32));
+                byte[] alicePublic = Curve25519.GetPublicKey(alicePrivate);
+
+                byte[] bobPrivate = Curve25519.ClampPrivateKey(TestHelpers.GetRandomBytes(random, 32));
+                byte[] bobPublic = Curve25519.GetPublicKey(bobPrivate);
+
+                byte[] aliceShared = Curve25519.GetSharedSecret(alicePrivate, bobPublic);
+
+                byte[] alicePublicWithBitToggled = TestHelpers.ToggleBitInKey(alicePublic, random);
+                byte[] bobShared = Curve25519.GetSharedSecret(bobPrivate, alicePublicWithBitToggled);
+
+                Assert.AreNotEqual(aliceShared, bobShared);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+[Curve25519](http://ed25519.cr.yp.to/) is an elliptic curve, 
+developed by [Dan Bernstein](http://cr.yp.to/djb.html), for fast Diffie-Hellman key agreement.
+
+#### Usage Example
+
+	// what Alice does
+	byte[] aliceRandomBytes = new byte[32];
+	RNGCryptoServiceProvider.Create().GetBytes(aliceRandomBytes);
+
+	byte[] alicePrivate = Curve25519.ClampPrivateKey(someRandomBytes);
+	byte[] alicePublic = Curve25519.GetPublicKey(alicePrivate);
+
+	// what Bob does
+	byte[] bobRandomBytes = new byte[32];
+	RNGCryptoServiceProvider.Create().GetBytes(bobRandomBytes);
+
+	byte[] bobPrivate = Curve25519.ClampPrivateKey(bobRandomBytes);
+	byte[] bobPublic = Curve25519.GetPublicKey(bobPrivate);
+
+	// what Alice does with Bob's public key
+	byte[] aliceShared = Curve25519.GetSharedSecret(alicePrivate, bobPublic);
+	
+	// what Bob does with Alice' public key
+	byte[] bobShared = Curve25519.GetSharedSecret(bobPrivate, alicePublic);
+	
+	// aliceShared == bobShared
