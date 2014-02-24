bce0a10acb752b5a40b0422f008d3b459ea5fbc0
Hans Wolff <hans.wolff.home@gmail.com>
2014-02-24 19:54:25 +0100

    measure time of GetSharedSecret

--- Curve25519.Tests/Curve25519TimingTests.cs
+++ Curve25519.Tests/Curve25519TimingTests.cs
@@ -13,16 +13,42 @@ namespace Elliptic.Tests
         public void Curve25519_GetPublicKey()
         {
             var millis = new List<long>();
-            for (int i = 0; i < 255; i++)
+            for (var i = 0; i < 255; i++)
             {
-                byte[] privateKey = Curve25519.ClampPrivateKey(TestHelpers.GetUniformBytes((byte)i, 32));
+                var privateKey = Curve25519.ClampPrivateKey(TestHelpers.GetUniformBytes((byte)i, 32));
                 Curve25519.GetPublicKey(privateKey);
 
-                Stopwatch stopwatch = Stopwatch.StartNew();
+                var stopwatch = Stopwatch.StartNew();
 
-                for (int j = 0; j < 100; j++)
+                for (var j = 0; j < 100; j++)
                 {
                     Curve25519.GetPublicKey(privateKey);
+                }
+
+                millis.Add(stopwatch.ElapsedMilliseconds);
+            }
+
+            var text = new StringBuilder();
+            foreach (var ms in millis)
+                text.Append(ms + ",");
+            Assert.Inconclusive(text.ToString());
+        }
+
+        [Test]
+        public void Curve25519_GetSharedSecret()
+        {
+            var millis = new List<long>();
+            for (var i = 0; i < 255; i++)
+            {
+                var privateKey = Curve25519.ClampPrivateKey(TestHelpers.GetUniformBytes((byte)i, 32));
+                var publicKey = Curve25519.GetPublicKey(privateKey);
+                Curve25519.GetSharedSecret(privateKey, publicKey);
+
+                var stopwatch = Stopwatch.StartNew();
+
+                for (var j = 0; j < 100; j++)
+                {
+                    Curve25519.GetSharedSecret(privateKey, publicKey);
                 }
 
                 millis.Add(stopwatch.ElapsedMilliseconds);
