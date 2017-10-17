2e1709d1908b1628d2da13e36d75a05000ae2e6a
Mun1z <mrmunz2012@gmail.com>
2017-10-17 19:02:03 -0300

    Updated to .net framework 4.7, code refactoring, updated unit tests.

---
+++ Curve25519.Tests/Curve25519TimingTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Elliptic.Tests
+{
+    [Explicit]
+    [TestFixture]
+    public class Curve25519TimingTests
+    {
+        [Test]
+        public void Curve25519_GetPublicKey()
+        {
+            var ticks = new List<long>();
+            for (var i = 0; i < 255; i++)
+            {
+                var stopwatch = Stopwatch.StartNew();
+
+                var privateKey = Curve25519.ClampPrivateKey(TestHelpers.GetUniformBytes((byte)i, 32));
+
+                for (var j = 0; j < 1000; j++)
+                {
+                    var publicKey = Curve25519.GetPublicKey(privateKey);
+                }
+
+                ticks.Add(stopwatch.ElapsedMilliseconds);
+            }
+
+            var min = long.MaxValue;
+            var max = long.MinValue;
+            foreach (var t in ticks)
+            {
+                if (t < min) min = t;
+                if (t > max) max = t;
+            }
+
+            Assert.Inconclusive("Min: {0}, Max: {1}", min, max);
+        }
+    }
+}
---
+++ Curve25519.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Elliptic.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Elliptic.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2013-2014 by Hans Wolff")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f68096d7-310d-4200-81a9-0443fb40ca29")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
